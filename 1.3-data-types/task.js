79378658160612db3dab1d83143f6318183e7cbf
alekkss <alekkss1993@yandex.ru>
2021-01-07 21:05:01 +0300

    сделал задание 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,15 +1,16 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     "use strict";
     let totalSum = amount - contribution;
-    percent = 1 / percent;
+    percent = percent / 100;
+    console.log(percent);
     percent = percent / 12;
     let currentDate = new Date();
     let term = (date.getFullYear() - currentDate.getFullYear()) * 12;
     console.log(percent);
 
-    let paymentMonth = totalSum * (percent + percent / (((1 + percent) ^ term) - 1));
+    let paymentMonth = totalSum * (percent + percent / (((1 + percent) ** term) - 1));
     let pay = paymentMonth * term;
-    pay = pay.toFixed(2);
+    pay = +pay.toFixed(2);
     if (pay == 0) {
         pay = 0;
     }
